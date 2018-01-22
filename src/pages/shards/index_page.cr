e1350cc4c14cc96bc4400cbec15b8441e6b57068
John Bannister <jdbann@gmail.com>
2018-01-22 13:40:28 +0000

    Extract shard detail to Shards::ShardComponent.render

--- src/pages/shards/index_page.cr
+++ src/pages/shards/index_page.cr
@@ -1,4 +1,6 @@
 class Shards::IndexPage < MainLayout
+  include Shards::ShardComponent
+
   needs shards : ShardQuery
   needs shard_form : ShardForm
 
@@ -10,35 +12,7 @@ class Shards::IndexPage < MainLayout
     h2 "Shards"
 
     @shards.each do |shard|
-      div class: "shard-details" do
-        h3 do
-          link shard.full_name, to: Show.with(id: shard.id)
-        end
-        div class: "shard-details-columns" do
-          div class: "shard-details-description" do
-            para "Created #{shard.repo_created_at.to_s("%-d %B %Y")}"
-            em do
-              para shard.description
-            end
-          end
-          div class: "shard-details-metadata" do
-            ul do
-              li do
-                text "#{shard.stargazers_count} stars"
-              end
-              li do
-                text "#{shard.watchers_count} watchers"
-              end
-              li do
-                text "#{shard.forks_count} forks"
-              end
-              li do
-                text "#{shard.subscribers_count} subscribers"
-              end
-            end
-          end
-        end
-      end
+      render(shard)
     end
   end
 
--- src/pages/shards/show_page.cr
+++ src/pages/shards/show_page.cr
@@ -1,28 +1,9 @@
 class Shards::ShowPage < MainLayout
+  include Shards::ShardComponent
+
   needs shard : Shard
 
   def inner
-    h1 @shard.full_name
-
-    para @shard.description
-
-    table do
-      tr do
-        td "Forks"
-        td @shard.forks_count
-      end
-      tr do
-        td "Stars"
-        td @shard.stargazers_count
-      end
-      tr do
-        td "Subscribers"
-        td @shard.subscribers_count
-      end
-      tr do
-        td "Watchers"
-        td @shard.watchers_count
-      end
-    end
+    render @shard
   end
 end
