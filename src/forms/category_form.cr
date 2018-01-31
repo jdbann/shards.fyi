1ec045bd0bffcf6836f171107fd1171b5ce5aab1
Edward Loveall <edward@edwardloveall.com>
2018-01-31 17:10:42 -0500

    Validate uniqueness of Category titles

--- src/forms/category_form.cr
+++ src/forms/category_form.cr
@@ -1,3 +1,13 @@
 class CategoryForm < Category::BaseForm
   allow title
+
+  def prepare
+    validate_uniqueness_of_title
+  end
+
+  def validate_uniqueness_of_title
+    if CategoryQuery.new.title.lower.is(title.value.to_s.downcase).size > 0
+      title.add_error "already exists"
+    end
+  end
 end
